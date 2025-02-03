63a1f75c8e4715b3443747e44c6cdf93e1f69c90
Daniil Baturyn <notfeardan@gmail.com>
2025-02-02 23:54:43 -0600

    Added extensibility

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const brushTime = 1500;
 const activeToolEl = document.getElementById("active-tool");
 const brushColorBtn = document.getElementById("brush-color");
 const brushIcon = document.getElementById("brush");
@@ -73,6 +74,10 @@ function switchToBrush() {
   displayBrushSize();
 }
 
+function brushSetTimeout(time) {
+  setTimeout(switchToBrush, time);
+}
+
 // Create Canvas
 function createCanvas() {
   canvas.width = window.innerWidth;
@@ -89,7 +94,7 @@ clearCanvasBtn.addEventListener("click", () => {
   drawnArray = [];
   // Active Tool
   activeToolEl.textContent = "Canvas Cleared";
-  setTimeout(switchToBrush, 1500);
+  brushSetTimeout(brushTime);
 });
 
 // // Draw what is stored in DrawnArray
@@ -169,7 +174,7 @@ saveStorageBtn.addEventListener("click", () => {
   localStorage.setItem("savedCanvas", JSON.stringify(drawnArray));
   // Active Tool
   activeToolEl.textContent = "Canvas Saved";
-  setTimeout(switchToBrush, 1500);
+  brushSetTimeout(brushTime);
 });
 
 // Load from Local Storage
@@ -179,7 +184,7 @@ loadStorageBtn.addEventListener("click", () => {
     restoreCanvas();
     // Active Tool
     activeToolEl.textContent = "Canvas Loaded";
-    setTimeout(switchToBrush, 1500);
+    brushSetTimeout(brushTime);
   }
 });
 
@@ -188,7 +193,7 @@ clearStorageBtn.addEventListener("click", () => {
   localStorage.removeItem("savedCanvas");
   // Active Tool
   activeToolEl.textContent = "Local Storage Cleared";
-  setTimeout(switchToBrush, 1500);
+  brushSetTimeout(brushTime);
 });
 
 // Download Image
@@ -197,7 +202,7 @@ downloadBtn.addEventListener("click", () => {
   downloadBtn.download = "paint-tool.jpeg";
   // Active Tool
   activeToolEl.textContent = "Image File Saved";
-  setTimeout(switchToBrush, 1500);
+  brushSetTimeout(brushTime);
 });
 
 // Event Listener
